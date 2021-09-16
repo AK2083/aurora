9915221540b4e140a3349f28a25ac7e86aee8e74
Adrian <ahelmchen.web@posteo.de>
2021-09-16 07:58:21 -0400

    correct code smell

--- karma.conf.js
+++ karma.conf.js
@@ -37,10 +37,9 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true,
-    browsers: ['ChromeHeadlessCI'],
+    browsers: ['Chrome', 'ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
