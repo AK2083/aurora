5da4b26e63b6266fef71c0ce9699608a3d5552fc
Adrian <ahelmchen.web@posteo.de>
2021-09-16 08:07:55 -0400

    init firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+    apiKey: "AIzaSyDTwvQ2VLJii_NQI3DfJEQnGGcwRXCpAII",
+    authDomain: "aurora-471d3.firebaseapp.com",
+    projectId: "aurora-471d3",
+    storageBucket: "aurora-471d3.appspot.com",
+    messagingSenderId: "583557406808",
+    appId: "1:583557406808:web:24cf3e220c8b06bd5fea2d",
+    measurementId: "G-N4MV16ZQMP"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:  {
+    apiKey: "AIzaSyDTwvQ2VLJii_NQI3DfJEQnGGcwRXCpAII",
+    authDomain: "aurora-471d3.firebaseapp.com",
+    projectId: "aurora-471d3",
+    storageBucket: "aurora-471d3.appspot.com",
+    messagingSenderId: "583557406808",
+    appId: "1:583557406808:web:24cf3e220c8b06bd5fea2d",
+    measurementId: "G-N4MV16ZQMP"
+  }
 };
 
 /*
